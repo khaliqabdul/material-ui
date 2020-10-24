a134568c3b3ef85fe029746b314433d73501eec0
Abdul Khaliq <abdulkhaliqazeemi@gmail.com>
2020-10-24 08:13:56 +0500

    updated App.js with my name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Updated by Abdul Khaliq
+          Updated by Abdul khaliq
         </p>
         <a
           className="App-link"
