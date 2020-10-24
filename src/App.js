57319924a0cc78640dd2525f231f6028a82d41dd
Abdul Khaliq <abdulkhaliqazeemi@gmail.com>
2020-10-24 13:07:27 +0500

    Changed default logo from App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Updated by Abdul khaliq
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>This is my first React App</h1>
 }
 
 export default App;
