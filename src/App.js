99bd9ee2f6489ad8534281ba89a4a8ae6bbf3e46
Abdul Khaliq <abdulkhaliqazeemi@gmail.com>
2020-10-25 19:10:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World from Abdul khaliq!</h1>      
+      <h1>Hello World from Abdul khaliq!</h1> 
+      <h2>Assalam-o-Alikum</h2>     
     </div>
   );
 }
